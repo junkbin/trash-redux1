041b9a52d3873ca6c4c8b8602c2580a2c655dea7
santosh <santosh.ece06@gmail.com>
2018-08-04 17:12:50 +0530

    init.

---
+++ src/Main.js
@@ -0,0 +1,16 @@
+/**
+ * Redux sample example @backend
+ */
+
+class Main{
+    static main(){
+        try{
+            console.log("Hello Redux!!!");
+        }catch(err){
+            console.error(err);
+        }
+    }
+}
+
+
+Main.main();
